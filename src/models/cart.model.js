a60f2728c5c58f074a321db6d501bac7f5196f8d
Ngọc Cường <cuongtech47@gmail.com>
2023-09-14 01:53:00 +0700

    fix prettier

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -5,24 +5,26 @@ const { model, Schema } = require("mongoose");
 const DOCUMENT_NAME = "Cart";
 const COLLECTION_NAME = "Carts";
 
-const cartSchema = new Schema({
-  cart_state: {
-    type: String,
-    required: true,
-    enum: ["active", "complated", "failed", "pending"],
+const cartSchema = new Schema(
+  {
+    cart_state: {
+      type: String,
+      required: true,
+      enum: ["active", "complated", "failed", "pending"],
+    },
+    cart_products: { type: Array, required: true, default: [] },
+    cart_count_product: { type: Number, default: 0 },
+    cart_userId: { type: Number, required: true },
   },
-  cart_products: { type: Array, required: true, default: [] },
-  cart_count_product: { type:Number , default: 0},
-  cart_userId : {type : Number , required : true},
-
-}, {
-  collection : COLLECTION_NAME,
-  timestamps: {
-    createdAt : "createdOn",
-    updatedAt: 'modifiedOn',
+  {
+    collection: COLLECTION_NAME,
+    timestamps: {
+      createdAt: "createdOn",
+      updatedAt: "modifiedOn",
+    },
   }
-});
+);
 
 module.exports = {
-  cart : model(DOCUMENT_NAME , cartSchema)
-}
+  cart: model(DOCUMENT_NAME, cartSchema),
+};
