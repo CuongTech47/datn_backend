8bd56227a9ee8e35680f9159ac535b1e6af91bfb
Ngọc Cường <cuongtech47@gmail.com>
2024-01-05 23:24:37 +0700

    change port fix

--- backend_nodejs/server.js
+++ backend_nodejs/server.js
@@ -1,7 +1,10 @@
 const app = require("./src/app");
 
 
-const PORT = process.env.DEV_APP_PORT
+// const PORT = process.env.DEV_APP_PORT
+
+const PORT = 3000
+
 
 const server = app.listen(PORT , ()=>{
     console.log(`Server is runing with port ${PORT}`)
